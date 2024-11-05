8910ab51cd68e2511da8e1bddedbd4676301f9a8
JordanPage21 <jpage58@gmail.com>
2024-11-05 11:03:41 -0500

    init

---
+++ src/main/java/com/jordan/page/projects/cardgamesimulator/CardgamesimulatorApplication.java
@@ -0,0 +1,13 @@
+package com.jordan.page.projects.cardgamesimulator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CardgamesimulatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CardgamesimulatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jordan/page/projects/cardgamesimulator/CardgamesimulatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jordan.page.projects.cardgamesimulator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CardgamesimulatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
